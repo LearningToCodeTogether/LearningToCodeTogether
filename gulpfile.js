b73cbcea6b795f0ab715deebb0c0dfd9c1b4fee7
Danny van der Jagt <dannyvanderjagt@gmail.com>
2018-06-14 15:05:12 +0200

    [Setup]

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const pug = require('gulp-pug')
+const plumber = require('gulp-plumber')
+
+const paths = {
+  src: {
+    styles: './src/styles/*.scss',
+    html: `./src/**/*.pug`,
+  },
+  public: {
+    default: './public',
+  },
+  assets: './assets/**/*.*',
+}
+
+gulp.task('styles', () =>
+  gulp
+    .src(paths.src.styles)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(paths.public.default))
+)
+
+gulp.task('html', () =>
+  gulp
+    .src(paths.src.html)
+    .pipe(plumber())
+    .pipe(pug())
+    .pipe(gulp.dest(paths.public.default))
+)
+
+gulp.task('assets', () =>
+  gulp
+    .src(paths.assets)
+    .pipe(gulp.dest(paths.public.default))
+)
+
+
+gulp.task('styles:watch', () => gulp.watch(paths.src.styles, ['styles']))
+gulp.task('html:watch', () => gulp.watch(paths.src.html, ['html']))
+gulp.task('assets:watch', () => gulp.watch(paths.assets, ['assets']))
+
+gulp.task('default', ['html', 'styles', 'assets', 'watch'])
+gulp.task('watch', ['html:watch', 'styles:watch', 'assets:watch'])
